c34a24cf373db4e01a3ecbe78170fbc7b445d2cb
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2018-09-02 22:27:36 -0500

    Avance 2

--- app/src/main/java/proyecto/aplicacion/wasi/wasi/Login.java
+++ app/src/main/java/proyecto/aplicacion/wasi/wasi/Login.java
@@ -1,13 +1,55 @@
 package proyecto.aplicacion.wasi.wasi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    private EditText usuario;
+    private EditText clave;
+    private Button ingresar;
+    private int perfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.botonIngresar : verificar(intent);
+        }
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Método que se encargará de dirigir a una diferente vista
+     * dependiendo del perfil del usuario
+     * @param intent
+     */
+    private void verificar(Intent intent){
+        switch (perfil){
+            case 1 : intent = new Intent(Login.this, Apoderado.class);
+        }
+    }
+
+    /**
+     * Recibe el dato del tipo de perfil que esta intentando acceder la persona
+     * que usa la aplicación
+     */
+    private void tipoPerfil(){
+        Intent inten = getIntent();
+        Bundle bun = inten.getExtras();
+
+        if(bun != null){
+            perfil = (int)bun.get("perfil");
+        }
     }
 }
--- app/src/main/java/proyecto/aplicacion/wasi/wasi/Perfil.java
+++ app/src/main/java/proyecto/aplicacion/wasi/wasi/Perfil.java
@@ -28,11 +28,14 @@ public class Perfil extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View view) {
         Intent intent = null;
         switch(view.getId()){
-            case R.id.botonApoderado : intent = new Intent(Perfil.this, Login.class);
+            case R.id.botonApoderado :  intent = new Intent(Perfil.this, Login.class);
+                                        intent.putExtra("perfil", 1);
                                         break;
-            case R.id.botonMovilidad : intent = new Intent(Perfil.this, Login.class);
+            case R.id.botonMovilidad :  intent = new Intent(Perfil.this, Login.class);
+                                        intent.putExtra("perfil", 2);
                                         break;
-            case R.id.botonRecogedor : intent = new Intent(Perfil.this, Login.class);
+            case R.id.botonRecogedor :  intent = new Intent(Perfil.this, Login.class);
+                                        intent.putExtra("perfil", 3);
                                         break;
         }
         startActivity(intent);
