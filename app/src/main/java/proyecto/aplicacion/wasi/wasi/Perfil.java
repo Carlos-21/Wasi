b27b3168f8993d3af8b59afd2aef522a70fa4f0d
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2018-09-19 16:32:20 -0500

    Avance7

--- app/src/main/java/proyecto/aplicacion/wasi/wasi/Perfil.java
+++ app/src/main/java/proyecto/aplicacion/wasi/wasi/Perfil.java
@@ -1,42 +1,46 @@
 package proyecto.aplicacion.wasi.wasi;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Perfil extends AppCompatActivity implements View.OnClickListener{
-    private Button botonApoderado;
-    private Button botonMovilidad;
-    private Button botonRecogedor;
+    private ImageView perfilApoderado;
+    private ImageView perfilRecogedor;
+    private ImageView perfilProfesor;
+    private ImageView perfilMovilidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        setTitle(R.string.cPerfil);
+        perfilApoderado = (ImageView)findViewById(R.id.perfilApoderado);
+        perfilApoderado.setOnClickListener(this);
+        perfilRecogedor = (ImageView)findViewById(R.id.perfilRecogedor);
+        perfilRecogedor.setOnClickListener(this);
+        perfilProfesor = (ImageView)findViewById(R.id.perfilProfesor);
+        perfilProfesor.setOnClickListener(this);
+        perfilMovilidad = (ImageView)findViewById(R.id.perfilMovilidad);
+        perfilMovilidad.setOnClickListener(this);
 
-        botonApoderado = (Button)findViewById(R.id.botonApoderado);
-        botonApoderado.setOnClickListener(this);
-        botonMovilidad = (Button)findViewById(R.id.botonMovilidad);
-        botonMovilidad.setOnClickListener(this);
-        botonRecogedor = (Button)findViewById(R.id.botonRecogedor);
-        botonRecogedor.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Intent intent = null;
         switch(view.getId()){
-            case R.id.botonApoderado :  intent = new Intent(Perfil.this, Login.class);
+            case R.id.perfilApoderado :  intent = new Intent(Perfil.this, Login.class);
                                         intent.putExtra("perfil", 1);
                                         break;
-            case R.id.botonMovilidad :  intent = new Intent(Perfil.this, Login.class);
+            case R.id.perfilMovilidad :  intent = new Intent(Perfil.this, Login.class);
                                         intent.putExtra("perfil", 2);
                                         break;
-            case R.id.botonRecogedor :  intent = new Intent(Perfil.this, Login.class);
+            case R.id.perfilRecogedor :  intent = new Intent(Perfil.this, Login.class);
                                         intent.putExtra("perfil", 3);
                                         break;
         }
